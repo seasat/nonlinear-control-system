d218a5604dd998a45ae80762e6611b2530e4e8b3
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-04-25 16:09:05 +0200

    add spacecraft class with an inertia tensor

---
+++ nonlinear_control_system/spacecraft.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class Spacecraft:
+    def __init__(self, inertia_tensor: np.mat) -> None:
+        """
+        Initialize the Spacecraft class with an inertia tensor.
+
+        :param inertia_tensor: The inertia tensor of the spacecraft.
+        """
+        assert inertia_tensor.shape == (3, 3), "Inertia tensor must be a 3x3 matrix"
+
+        self.inertia_tensor = inertia_tensor
