ecf8adb5bdbd4ca38ccaf3093b6f3b911e827af3
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-14 13:52:52 +0200

    move to function run if name is main

--- nonlinear_control_system/__main__.py
+++ nonlinear_control_system/__main__.py
@@ -5,18 +5,24 @@ from spacecraft import Spacecraft
 from quantity import Quantity as Qty
 from simulation import Simulation
 
-# settings
-INERTIA_TENSOR = Qty.matrix(
-    np.asmatrix([
-        [124.531, 0.0, 0.0],
-        [0.0, 124.586, 0.0],
-        [0.0, 0.0, 1.704]
-    ]), 'kg m2'
-)
-SIMULATION_DURATION = Qty(1000, 's')
-SAMPLE_TIME = Qty(0.1, 's')
+
+def main():
+    # settings
+    INERTIA_TENSOR = Qty.matrix(
+        np.asmatrix([
+            [124.531, 0.0, 0.0],
+            [0.0, 124.586, 0.0],
+            [0.0, 0.0, 1.704]
+        ]), 'kg m2'
+    )
+    SIMULATION_DURATION = Qty(1000, 's')
+    SAMPLE_TIME = Qty(0.1, 's')
+
+
+    if __name__ == "__main__":
+        sc = Spacecraft(INERTIA_TENSOR, None)
+        simulation = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME)
 
 
 if __name__ == "__main__":
-    sc = Spacecraft(INERTIA_TENSOR, None)
-    simulation = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME)
+    SystemExit(main())
