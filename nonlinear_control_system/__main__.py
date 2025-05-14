aafc927263ba995a2683700aff76bf116883cd70
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-14 18:54:38 +0200

    set initial spacecraft attitude to 30 degrees all axes

--- nonlinear_control_system/__main__.py
+++ nonlinear_control_system/__main__.py
@@ -3,19 +3,21 @@ import numpy as np
 
 from spacecraft import Spacecraft
 from simulation import Simulation
+from attitude import YawPitchRoll
 
 
 def main():
     # settings
     INERTIA_TENSOR = np.asmatrix([
-            [124.531, 0.0, 0.0],
-            [0.0, 124.586, 0.0],
-            [0.0, 0.0, 1.704]
-        ])
+        [124.531, 0.0, 0.0],
+        [0.0, 124.586, 0.0],
+        [0.0, 0.0, 1.704]
+    ])
+    INITIAL_ATTITUDE = YawPitchRoll(np.deg2rad(30), np.deg2rad(30), np.deg2rad(30))
     SIMULATION_DURATION = 1000 # s
     SAMPLE_TIME = 0.1 # s
 
-    sc = Spacecraft(INERTIA_TENSOR, None)
+    sc = Spacecraft(INERTIA_TENSOR, INITIAL_ATTITUDE)
     simulation = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME)
 
 
