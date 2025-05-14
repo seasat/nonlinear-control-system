c63837c07987cfc7e335f139e14facfc34f5eea7
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-14 20:59:26 +0200

    set attitude to None instead

--- nonlinear_control_system/simulation.py
+++ nonlinear_control_system/simulation.py
@@ -37,7 +37,8 @@ class Simulation:
         """
         target_attitude_commands = sorted(target_attitude_commands.items())
         command_iterator = iter(target_attitude_commands)
-        current_command_attitude = 0
+        current_command_attitude = None
+        command = target_attitude_commands[0]
         for idx, time in enumerate(self.times):
             if time >= command[0]:
                 current_command_attitude = command[1]
