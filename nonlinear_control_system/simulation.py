28000545a924928ab97e0c5d536dbf04a015ae0c
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-14 21:08:50 +0200

    define a plot attitudes method

--- nonlinear_control_system/simulation.py
+++ nonlinear_control_system/simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from spacecraft import Spacecraft
 from attitude import Attitude
@@ -48,3 +49,27 @@ class Simulation:
                     break
 
             self.target_attitudes[idx] = current_command_attitude
+    
+    def plot_attitudes(self) -> None:
+        """
+        Plot the attitudes over time.
+        """
+        fig, ax = plt.subplots(1, 1, figsize=(6, 2.5), tight_layout=True)
+
+        yaws = np.array([attitude.yaw for attitude in self.attitudes])
+        ax.plot(self.times, yaws, label="Yaw")
+        pitches = np.array([attitude.pitch for attitude in self.attitudes])
+        ax.plot(self.times, pitches, label="Pitch")
+        rolls = np.array([attitude.roll for attitude in self.attitudes])
+        ax.plot(self.times, rolls, label="Roll")
+
+        ax.set_color_cycle(None)
+        command_yaws = np.array([attitude.yaw for attitude in self.target_attitudes])
+        ax.plot(self.times, command_yaws, label="Command Yaw", linestyle='--')
+        command_pitches = np.array([attitude.pitch for attitude in self.target_attitudes])
+        ax.plot(self.times, command_pitches, label="Command Pitch", linestyle='--')
+        command_rolls = np.array([attitude.roll for attitude in self.target_attitudes])
+        ax.plot(self.times, command_rolls, label="Command Roll", linestyle='--')
+
+        ax.set_xlabel("Time $[\mathrm{s}]$")
+        ax.set_ylabel("Attitude $[\mathrm{rad}]$")
