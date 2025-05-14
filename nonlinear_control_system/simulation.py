2bbb84512f7b2548f1344323cfd83235e6df3299
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-14 20:25:08 +0200

    add calculate target attitude array method

--- nonlinear_control_system/simulation.py
+++ nonlinear_control_system/simulation.py
@@ -1,18 +1,21 @@
 import numpy as np
 
 from spacecraft import Spacecraft
+from attitude import Attitude
 
 
 class Simulation:
-    def __init__(self, spacecraft: Spacecraft, duration: float, sample_time: float) -> None:
+    def __init__(self, spacecraft: Spacecraft, duration: float, sample_time: float, target_attitude_commands: dict[float: Attitude]) -> None:
         """
         Initialize the Simulation class with a spacecraft, duration, and sample time.
 
         :param spacecraft: The spacecraft to be simulated.
         :param duration: The total duration of the simulation in seconds.
         :param sample_time: The time interval between each simulation step in seconds.
+        :param target_attitudes: A dictionary of target attitudes with time as the key and attitude as the value.
         """
         assert isinstance(spacecraft, Spacecraft), "Spacecraft must be an instance of Spacecraft"
+        assert isinstance(target_attitude_commands, dict), "Target attitudes must be a dictionary"
 
         self.spacecraft = spacecraft
         self.duration = duration
@@ -25,3 +28,23 @@ class Simulation:
         self.attitudes = np.zeros(self.sample_points, dtype=type(spacecraft.attitude)) # use same attitude type as spacecraft stores
         self.angular_velocities = np.zeros_like(self.attitudes)
         self.target_attitudes = np.zeros_like(self.attitudes)
+
+        self._calculate_target_attitudes(target_attitude_commands)
+    
+    def _calculate_target_attitudes(self, target_attitude_commands: dict[float: Attitude]) -> None:
+        """
+        Calculate the target attitudes based on the target attitude commands.
+        """
+        target_attitude_commands = sorted(target_attitude_commands.items())
+        next_command = iter(target_attitude_commands)
+        current_command_attitude = next_command[1]
+        next(next_command)
+        for idx, time in enumerate(self.times):
+            if time >= next_command[0]:
+                current_command_attitude = next_command[1]
+                try:
+                    next(next_command)
+                except StopIteration:
+                    break
+
+            self.target_attitudes[idx] = current_command_attitude
