ad39e10cc2b3dc387ab8c4a52bf495e4fcfda949
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-14 14:00:21 +0200

    remove quantity module

--- nonlinear_control_system/__main__.py
+++ nonlinear_control_system/__main__.py
@@ -2,21 +2,18 @@
 import numpy as np
 
 from spacecraft import Spacecraft
-from quantity import Quantity as Qty
 from simulation import Simulation
 
 
 def main():
     # settings
-    INERTIA_TENSOR = Qty.matrix(
-        np.asmatrix([
+    INERTIA_TENSOR = np.asmatrix([
             [124.531, 0.0, 0.0],
             [0.0, 124.586, 0.0],
             [0.0, 0.0, 1.704]
-        ]), 'kg m2'
-    )
-    SIMULATION_DURATION = Qty(1000, 's')
-    SAMPLE_TIME = Qty(0.1, 's')
+        ])
+    SIMULATION_DURATION = 1000 # s
+    SAMPLE_TIME = 0.1 # s
 
     sc = Spacecraft(INERTIA_TENSOR, None)
     simulation = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME)
