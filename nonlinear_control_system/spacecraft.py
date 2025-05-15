6a1da662a0f4a2100b94ac81c7734bc4225ac33f
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-15 07:53:17 +0200

    change angular velocity to an np matrix

--- nonlinear_control_system/spacecraft.py
+++ nonlinear_control_system/spacecraft.py
@@ -4,7 +4,7 @@ from attitude.quaternion import Attitude
 
 
 class Spacecraft:
-    def __init__(self, inertia_tensor: np.matrix, attitude: Attitude, angular_velocity: Attitude) -> None:
+    def __init__(self, inertia_tensor: np.matrix, attitude: Attitude, angular_velocity: np.matrix) -> None:
         """
         Initialize the Spacecraft class with an inertia tensor and attitude in form of a quaternion.
 
@@ -12,7 +12,7 @@ class Spacecraft:
         """
         assert inertia_tensor.shape == (3, 3), "Inertia tensor must be a 3x3 matrix"
         assert isinstance(attitude, Attitude), "Attitude must be an Attitude object"
-        assert isinstance(angular_velocity, Attitude), "Angular velocity must be an Attitude object"
+        assert attitude.shape == (3, 1), "Attitude must be a 3x1 matrix"
 
         self.inertia_tensor = inertia_tensor
         self.attitude = attitude
