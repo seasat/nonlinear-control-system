a9d189a4a4f2b41a1d21fa3dd7e58cbfd4cfa58d
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-19 18:27:26 +0200

    angular velocity is a 3x1 matrix

--- nonlinear_control_system/spacecraft.py
+++ nonlinear_control_system/spacecraft.py
@@ -12,7 +12,7 @@ class Spacecraft:
         """
         assert inertia_tensor.shape == (3, 3), "Inertia tensor must be a 3x3 matrix"
         assert isinstance(attitude, Attitude), "Attitude must be an Attitude object"
-        assert attitude.shape == (3, 1), "Attitude must be a 3x1 matrix"
+        assert angular_velocity.shape == (3, 1), "Attitude must be a 3x1 matrix"
 
         self.inertia_tensor = inertia_tensor
         self.attitude = attitude
