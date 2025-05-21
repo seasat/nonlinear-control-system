e677da5c470122df493f69c206330505a23f1db6
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-21 18:23:10 +0200

    add rk4 integrator definition

---
+++ nonlinear_control_system/integrator.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def rk4(derivative: function, state: np.ndarray, time: float, time_step: float, *args):
+    """Runge-Kutta 4th order integration method."""
+    k1 = derivative(state, time, *args)
+    k2 = derivative(state + 0.5 * time_step * k1, time + 0.5 * time_step, *args)
+    k3 = derivative(state + 0.5 * time_step * k2, time + 0.5 * time_step, *args)
+    k4 = derivative(state + time_step * k3, time + time_step, *args)
+    
+    return state + (time_step / 6) * (k1 + 2*k2 + 2*k3 + k4)
