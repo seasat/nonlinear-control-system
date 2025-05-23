716e4d6d0d23c099e3a42601b336610a862aea72
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-23 21:13:23 +0200

    add rotating frame to rotational dynamics

--- nonlinear_control_system/dynamics.py
+++ nonlinear_control_system/dynamics.py
@@ -3,9 +3,12 @@ import numpy as np
 from attitude import YawPitchRoll, AngularVelocity
 
 
-def calculate_angular_acceleration(angular_velocity: np.ndarray, inertia_tensor: np.matrix, torque: np.ndarray) -> np.ndarray:
+def calculate_angular_acceleration(angular_velocity: np.ndarray, inertia_tensor: np.matrix, torque: np.ndarray, mean_motion: float) -> np.ndarray:
+    lvlh_velocity = np.array([[0], [-mean_motion], [0]])
+    inertial_velocity = angular_velocity + lvlh_velocity
     angular_acceleration = np.linalg.inv(inertia_tensor) @ (torque - np.cross(
-        angular_velocity.flatten(), (inertia_tensor @ angular_velocity).flatten()
+        inertial_velocity.flatten(),
+        (inertia_tensor @ inertial_velocity).flatten()
     ).reshape(3, 1))
     return angular_acceleration
 
@@ -14,7 +17,7 @@ def calculate_state_change(state: np.ndarray, time: None, inertia_tensor: np.nda
     attitude = YawPitchRoll([roll, pitch, yaw])
     angular_velocity = state[3:6]
 
-    angular_acceleration = calculate_angular_acceleration(angular_velocity, inertia_tensor, torque)
+    angular_acceleration = calculate_angular_acceleration(angular_velocity, inertia_tensor, torque, mean_motion)
     ypr_rates = AngularVelocity.to_ypr_rates(angular_velocity, attitude, mean_motion)
 
     state_change = np.concatenate((ypr_rates, angular_acceleration))
