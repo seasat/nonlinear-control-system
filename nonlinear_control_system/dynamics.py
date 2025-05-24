67a644a4cad0cbaf73c465a996d8b5d26d2625b6
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-05-24 10:39:32 +0200

    implement basic equations for linearized system

--- nonlinear_control_system/dynamics.py
+++ nonlinear_control_system/dynamics.py
@@ -22,3 +22,27 @@ def calculate_state_change(state: np.ndarray, time: None, inertia_tensor: np.nda
 
     state_change = np.concatenate((ypr_rates, angular_acceleration))
     return state_change
+
+def get_linearized_system(inertia_tensor: np.matrix, mean_motion: float) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Get the system matrices A and B for the linearized system
+    dy/dt = A @ y + B @ u
+    """
+    J_INV = np.linalg.inv(inertia_tensor)
+
+    a = np.zeros((6, 6))
+    a[0:3, 3:6] = np.eye(3)  # Identity matrix for angular velocity
+    a[0, 1] = mean_motion
+    print(f"{a=}")
+    orbital_coupling = np.array([
+        [0, 0, -mean_motion * (inertia_tensor[2,2] + inertia_tensor[1,1])],
+        [0, 0, 0],
+        [mean_motion * (inertia_tensor[0,0] + inertia_tensor[1,1]), 0, 0],
+    ])
+    a[3:6, 3:6] = -np.linalg.inv(inertia_tensor) @ orbital_coupling
+    print(f"{a=}")
+
+    b = np.zeros((6, 3))
+    # control torque affect the angular velocity
+    b[3:6, 0:3] = J_INV
+    print(f"{b=}")
