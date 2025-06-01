66cbf36c24a13aae89db2df96aa727299bf3adc3
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-06-01 20:34:55 +0200

    add option to use tex for label evaluation

--- nonlinear_control_system/__main__.py
+++ nonlinear_control_system/__main__.py
@@ -39,6 +39,8 @@ def main():
     NATURAL_FREQUENCY = 0.1 # rad/s
     DAMPING_RATIO = 0.99
 
+    #plt.rc('text', usetex=True)
+
     system_poles = PDController.calculate_poles(INERTIA_TENSOR, NATURAL_FREQUENCY, DAMPING_RATIO)
     sc = Spacecraft(INERTIA_TENSOR, INITIAL_ATTITUDE, BodyRates([0, 0, 0]), ORBIT)
 
@@ -48,23 +50,23 @@ def main():
     simulation.plot_attitude_errors()
     simulation.plot_control_torques()
 
-    ndi_controller = NDIController(sc, DISTURBANCE_TORQUE, system_poles)
-    sc.set_state(INITIAL_ATTITUDE, BodyRates([0, 0, 0]))  # Reset attitude and angular velocity for NDI simulation
-    simulation_ndi = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME, DISTURBANCE_TORQUE, ATTITUDE_COMMANDS, ndi_controller)
-    simulation_ndi.plot_attitudes()
-    simulation_ndi.plot_attitude_errors()
+    #ndi_controller = NDIController(sc, DISTURBANCE_TORQUE, system_poles)
+    #sc.set_state(INITIAL_ATTITUDE, BodyRates([0, 0, 0]))  # Reset attitude and angular velocity for NDI simulation
+    #simulation_ndi = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME, DISTURBANCE_TORQUE, ATTITUDE_COMMANDS, ndi_controller)
+    #simulation_ndi.plot_attitudes()
+    #simulation_ndi.plot_attitude_errors()
 
-    tss_controller = TSSController(sc, DISTURBANCE_TORQUE, system_poles)
-    sc.set_state(INITIAL_ATTITUDE, BodyRates([0, 0, 0]))  # Reset attitude and angular velocity for TSS simulation
-    simulation_tss = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME, DISTURBANCE_TORQUE, ATTITUDE_COMMANDS, tss_controller)
-    simulation_tss.plot_attitudes()
-    simulation_tss.plot_attitude_errors()
+    #tss_controller = TSSController(sc, DISTURBANCE_TORQUE, system_poles)
+    #sc.set_state(INITIAL_ATTITUDE, BodyRates([0, 0, 0]))  # Reset attitude and angular velocity for TSS simulation
+    #simulation_tss = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME, DISTURBANCE_TORQUE, ATTITUDE_COMMANDS, tss_controller)
+    #simulation_tss.plot_attitudes()
+    #simulation_tss.plot_attitude_errors()
 
-    indi_controller = INDIController(sc, DISTURBANCE_TORQUE, system_poles)
-    sc.set_state(INITIAL_ATTITUDE, BodyRates([0, 0, 0]))  # Reset attitude and angular velocity for INDI simulation
-    simulation_indi = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME, DISTURBANCE_TORQUE, ATTITUDE_COMMANDS, indi_controller)
-    simulation_indi.plot_attitudes()
-    simulation_indi.plot_attitude_errors()
+    #indi_controller = INDIController(sc, DISTURBANCE_TORQUE, system_poles)
+    #sc.set_state(INITIAL_ATTITUDE, BodyRates([0, 0, 0]))  # Reset attitude and angular velocity for INDI simulation
+    #simulation_indi = Simulation(sc, SIMULATION_DURATION, SAMPLE_TIME, DISTURBANCE_TORQUE, ATTITUDE_COMMANDS, indi_controller)
+    #simulation_indi.plot_attitudes()
+    #simulation_indi.plot_attitude_errors()
 
     plt.show()
 
