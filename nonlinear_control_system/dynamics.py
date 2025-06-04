08eb5c4a73b2330d90ec3191b919cc04b1d046f7
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-06-04 19:20:31 +0200

    update to match new calculate derivative function

--- nonlinear_control_system/dynamics.py
+++ nonlinear_control_system/dynamics.py
@@ -18,8 +18,8 @@ def calculate_state_change(state: np.ndarray, time: None, inertia_tensor: np.nda
     angular_velocity = state[3:6]
 
     angular_acceleration = calculate_angular_acceleration(angular_velocity, inertia_tensor, torque, mean_motion)
-    ypr_rates = angular_velocity.to_ypr_rates(attitude, mean_motion)
+    attitude_rates = attitude.calculate_derivative(angular_velocity, mean_motion)
 
-    state_change = np.concatenate((ypr_rates, angular_acceleration))
+    state_change = np.concatenate((attitude_rates, angular_acceleration))
     return state_change
 
