384be565c6b5f5c95e7262341910602c5d544200
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-06-04 17:30:19 +0200

    make parameter name consistent

--- nonlinear_control_system/attitude/attitude.py
+++ nonlinear_control_system/attitude/attitude.py
@@ -7,7 +7,7 @@ class Attitude:
     def __init__(self) -> None:
         pass
 
-    def calculate_derivative(self, body_rates: BodyRates, n: float) -> np.ndarray:
+    def calculate_derivative(self, body_rates: BodyRates, mean_motion: float) -> np.ndarray:
         """
         Calculate the derivative of the attitude based on body rates and mean motion.
         """
