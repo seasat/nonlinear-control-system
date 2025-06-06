c5adb12ffa861887f05d275d7a7b157ff22f1c0c
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-06-06 14:59:58 +0200

    add to vector funcionality

--- nonlinear_control_system/state_vector.py
+++ nonlinear_control_system/state_vector.py
@@ -4,13 +4,16 @@ import numpy as np
 from attitude import Attitude
 import dynamics
 
-class StateVector(np.ndarray):
+class StateVector:
     def __init__(self, attitude: Attitude, angular_rates: np.ndarray) -> None:
         assert isinstance(attitude, Attitude)
         assert angular_rates.shape == (3, 1), "angular rates must be a 3x1 array"
 
         self.attitude = attitude
         self.angular_rates = angular_rates
+    
+    def to_vector(self) -> np.ndarray:
+        return np.vstack([self.attitude.to_vector(), self.angular_rates])
 
     @staticmethod
     def calculate_state_derivative(state: StateVector, time: None, inertia_tensor: np.ndarray, torque: np.ndarray, mean_motion: float) -> np.ndarray:
