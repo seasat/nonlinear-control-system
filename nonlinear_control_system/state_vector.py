006af0f0d5cf305e820996df0a94b0d2830f0dcf
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-06-06 13:07:59 +0200

    setup a state vector class to handle integration independent of attitude representation

---
+++ nonlinear_control_system/state_vector.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+import numpy as np
+
+from attitude import Attitude
+import dynamics
+
+class StateVector(np.ndarray):
+    def __init__(self, attitude: Attitude, angular_rates: np.ndarray) -> None:
+        assert isinstance(attitude, Attitude)
+        assert angular_rates.shape == (3, 1), "angular rates must be a 3x1 array"
+
+        self.attitude = attitude
+        self.angular_rates = angular_rates
+
+    @staticmethod
+    def calculate_state_derivative(state: StateVector, time: None, inertia_tensor: np.ndarray, torque: np.ndarray, mean_motion: float) -> np.ndarray:
+        attitude_rates = state.attitude.calculate_derivative(state.angular_rates, mean_motion)
+        angular_accelerations = dynamics.calculate_angular_acceleration(state.angular_rates, inertia_tensor, torque, mean_motion)
+        return np.vstack([attitude_rates, angular_accelerations])
