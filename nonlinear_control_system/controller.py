44110dc275458b230c933f6887ec998269b4c3ee
Lars Blümler <lars.bluemler@ksat-stuttgart.de>
2025-06-07 15:13:21 +0200

    slice first three components for control and add a 0 q4 rate to convert to angular velocity

--- nonlinear_control_system/controller.py
+++ nonlinear_control_system/controller.py
@@ -193,8 +193,10 @@ class TSSController(Controller):
         attitude_error = attitude_error.to_vector()  # convert to vector for calculations
 
         # outer loop
-        target_ypr_rates: np.ndarray = -self.outer_loop_gains @ attitude_error
-        target_angular_velocity = self.sc.attitude.derivative_to_body_rates(target_ypr_rates, self.sc.orbit.mean_motion)
+        target_angular_rates: np.ndarray = -self.outer_loop_gains @ attitude_error[0:3]  # only use first three components for control
+        if isinstance(self.sc.attitude, Quaternion):
+            target_angular_rates = np.vstack([target_angular_rates, 0])  # add zero for quaternion case
+        target_angular_velocity = self.sc.attitude.derivative_to_body_rates(target_angular_rates, self.sc.orbit.mean_motion)
 
         # inner loop
         angular_velocity_error = self.sc.angular_velocity - target_angular_velocity  # Δω = ω - ω_d
